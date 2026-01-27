9f723889202bd922b18275b16c316b9d8e9abb0c
seyeong <seyoung4503@gmail.com>
2026-01-27 21:41:50 +0900

    feat : graph feature 수정

--- autograd/visualize.py
+++ autograd/visualize.py
@@ -7,7 +7,7 @@ def trace(root):
     def build(v):
         if v not in nodes:
             nodes.add(v)
-            for child in v.prev:
+            for child in v._prev:
                 edges.add((child, v))
                 build(child)
 
@@ -24,7 +24,7 @@ def draw_dot(root, format='svg', rankdir='LR'):
     dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
     
     for n in nodes:
-        dot.node(name=str(id(n)), label = "{ data %.4f | grad %.4f }" % (n.data, n.grad), shape='record')
+        dot.node(name=str(id(n)), label = "{ %s | data %.4f | grad %.4f }" % (n.label,n.data, n.grad), shape='record')
         if n._op:
             dot.node(name=str(id(n)) + n._op, label=n._op)
             dot.edge(str(id(n)) + n._op, str(id(n)))
