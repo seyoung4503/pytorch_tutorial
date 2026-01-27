a58346bdd790ead4e8816b3f960472fd407a4764
seyeong <seyoung4503@gmail.com>
2026-01-27 18:07:10 +0900

    feat : tanh, label 추가

--- autograd/value.py
+++ autograd/value.py
@@ -1,8 +1,10 @@
 class Value:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
-        self._children = _children
+        self.grad = 0.0
+        self._prev = set(_children)
         self._op = _op
+        self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
@@ -13,6 +15,12 @@ class Value:
     def __mul__(self, other):
         return Value(self.data * other.data, (self, other), _op='*')
     
+    def tanh(self):
+        import math
+        t = (math.exp(2 * self.data) - 1) / (math.exp(2 * self.data) + 1)
+        return Value(t, (self,), _op='tanh')
+    
+    
 if __name__ == "__main__":
     a = Value(10)
     b = Value(20)
@@ -22,3 +30,8 @@ if __name__ == "__main__":
     e = a + b*c
 
     print(e)
+
+    import visualize as viz
+    dot = viz.draw_dot(e)
+
+    dot.render('graphtree', view=True)
